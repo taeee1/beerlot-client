0db4a9ceaed90c816fe5ede6475c0b980c7d497c
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-09-17 17:20:18 +0900

    Feature/th arrange chakra theme (#9)
    * feat: color theme added
    
    * feat: text styling added
    
    * feat: chakra provider theme added

---
+++ theme.ts
@@ -0,0 +1,72 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#ffffff",
+      },
+    },
+  },
+
+  colors: {
+    White: {
+      100: "#FFFFFF",
+      200: "#FDF9EA",
+    },
+    Black: "#000000",
+    Orange: {
+      100: "#FEA801",
+      200: "#FF6B00",
+    },
+    Blue: {
+      100: "#52D5F2",
+    },
+    Yellow: {
+      100: "rgba(255, 229, 128, 0.8)",
+      200: "#FAD12B",
+    },
+    Gray: {
+      100: "rgba(97, 100, 107, 0.5)",
+      200: "#61646B",
+    },
+  },
+  textStyles: {
+    h1: {
+      fontFmily: "Roboto",
+      fontWeight: "700",
+      fontSize: "22px",
+      letterSpacing: "0.01px",
+    },
+    h2: {
+      fontFmily: "Roboto",
+      fontWeight: "500",
+      fontSize: "16x",
+      letterSpacing: "0.01px",
+    },
+    h2_bold: {
+      fontFmily: "Roboto",
+      fontWeight: "700",
+      fontSize: "16x",
+      letterSpacing: "0.01px",
+    },
+    h3: {
+      fontFmily: "Roboto",
+      fontWeight: "500",
+      fontSize: "12x",
+      letterSpacing: "0.01px",
+    },
+    h3_bold: {
+      fontFmily: "Roboto",
+      fontWeight: "700",
+      fontSize: "12x",
+      letterSpacing: "0.01px",
+    },
+    body: {
+      fontFmily: "Roboto",
+      fontWeight: "400",
+      fontSize: "15x",
+      letterSpacing: "0.01px",
+    },
+  },
+});
