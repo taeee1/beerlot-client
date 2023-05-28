4e2bbeb55dfbdbbdc2b40ff6a6924e2879aee45c
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-05-29 01:14:44 +0900

    feat: refetch user info

--- src/components/account/user-info/Profile.tsx
+++ src/components/account/user-info/Profile.tsx
@@ -4,11 +4,17 @@ import Cookies from "js-cookie";
 import EditProfileButton from "./EditProfileButton";
 import InfoContainer from "./InfoContainer";
 import MessageContainer from "./MessageContainer";
+import {useEffect} from "react";
+import {useQueries} from "react-query";
 
 const Profile = () => {
   const accessToken = Cookies.get("beerlot-oauth-auth-request");
   const userQuery = useUserInfoQuery(accessToken ?? "");
   const {image_url, username, status_message} = userQuery?.data ?? {};
+
+  useEffect(() => {
+    userQuery.refetch();
+  }, []);
 
   return (
     <VStack pt={0} px="20px" pb="10px">
