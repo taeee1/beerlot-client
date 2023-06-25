db7f801d7f69103f2d2fcbd8668d76d1ab3ef9e2
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-06-26 00:22:54 +0900

    feat: default image added

--- src/components/account/user-info/Profile.tsx
+++ src/components/account/user-info/Profile.tsx
@@ -1,11 +1,10 @@
 import {useUserInfoQuery} from "@/../hooks/query/useUserQuery";
 import {VStack} from "@chakra-ui/react";
 import Cookies from "js-cookie";
+import {useEffect} from "react";
 import EditProfileButton from "./EditProfileButton";
 import InfoContainer from "./InfoContainer";
 import MessageContainer from "./MessageContainer";
-import {useEffect} from "react";
-import {useQueries} from "react-query";
 
 const Profile = () => {
   const accessToken = Cookies.get("beerlot-oauth-auth-request");
@@ -17,7 +16,7 @@ const Profile = () => {
 
   return (
     <VStack pt={0} px="20px" pb="10px">
-      <InfoContainer imageSrc={image_url} />
+      <InfoContainer imageSrc={image_url || "/images/default-profile.png"} />
       <MessageContainer nickName={username} bio={status_message} />
       <EditProfileButton />
     </VStack>
