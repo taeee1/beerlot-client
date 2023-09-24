cc3b357e5848396de82654ff0e9e096c3719ef0b
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-09-24 15:29:14 +0900

    feat: display like icon on beer detail page

--- src/components/details/DetailInfo.tsx
+++ src/components/details/DetailInfo.tsx
@@ -1,14 +1,21 @@
-import {Box, Center, HStack, Image, Text, VStack} from "@chakra-ui/react";
-import {useState} from "react";
-import {LeftBackBeerNameRightHeart} from "../shared/Headers/LeftBackBeerNameRightHeart";
-import {LeftBackTitle} from "../shared/Headers/LeftBackTitle";
-import {Rating} from "../shared/Rating";
-import {useToast} from "@chakra-ui/react";
-import {useMutation} from "react-query";
+import { roundToDecimal } from "@/../utils/number";
+import { Box, Center, HStack, Text, VStack, useToast } from "@chakra-ui/react";
 import axios from "axios";
-import {LikeButton} from "../shared/LikeButton";
-import {CommonBeerImage} from "../shared/CommonBeerImage/CommonBeerImage";
-import {roundToDecimal} from "@/../utils/number";
+import Cookies from "js-cookie";
+import React, { useMemo, useState } from "react";
+import { useMutation } from "react-query";
+import {
+  useBeerDislikeMutation,
+  useBeerLikeMutation,
+} from "../../../hooks/query/useBeerLikeMutation";
+import { useUserBeersQuery } from "../../../hooks/query/useUserQuery";
+import { BeerResponseType } from "../../../typedef/server/beer";
+import { CommonBeerImage } from "../shared/CommonBeerImage/CommonBeerImage";
+import { LeftBackBeerNameRightHeart } from "../shared/Headers/LeftBackBeerNameRightHeart";
+import { LeftBackTitle } from "../shared/Headers/LeftBackTitle";
+import { LikeButton } from "../shared/LikeButton";
+import { Rating } from "../shared/Rating";
+
 interface DetailInfoProps {
   beerName: string;
   volume: number;
@@ -29,24 +36,37 @@ export const DetailInfo: React.FC<DetailInfoProps> = ({
   rate,
 }) => {
   const [didPassStar, setDidPassStar] = useState(false);
-  const [isLiked, setIsLiked] = useState(false);
   const rateToUse = roundToDecimal(rate);
-  const id = "test-toast";
-  const toastTitle = isLiked
+  const toastId = "test-toast";
+  const accessToken = Cookies.get("beerlot-oauth-auth-request") ?? "";
+  const userBeersQuery = useUserBeersQuery(accessToken);
+  const likedBeersList = userBeersQuery?.data?.contents;
+  const likedBeerIds = useMemo(
+    () => likedBeersList?.map((item: BeerResponseType) => item.id),
+    [likedBeersList]
+  );
+  const isLikedBeer = likedBeerIds?.includes(beerId);
+
+  const likeBeerMutation = useBeerLikeMutation({
+    onSuccess: () => {
+      userBeersQuery.refetch();
+    },
+  });
+
+  const dislikeBeerMutation = useBeerDislikeMutation({
+    onSuccess: () => {
+      userBeersQuery.refetch();
+    },
+  });
+
+  const toastTitle = isLikedBeer
     ? "좋아요한 맥주에서 삭제했어요!"
     : "좋아요한 맥주에 추가했어요!";
 
-  const likeBeer = useMutation((beerId: number) =>
-    axios.post(`/api/v1/beers/${beerId}/likes`)
-  );
-
-  const dislikeBeer = useMutation((beerId: number) =>
-    axios.delete(`/api/v1/beers/${beerId}/likes`)
-  );
   const toast = useToast({
     position: "bottom",
     title: toastTitle,
-    id: id,
+    id: toastId,
     isClosable: true,
     duration: 3000,
     containerStyle: {
@@ -69,9 +89,12 @@ export const DetailInfo: React.FC<DetailInfoProps> = ({
   });
 
   const handleClickLike = () => {
-    setIsLiked(!isLiked);
     toast();
-    isLiked ? dislikeBeer.mutate(beerId) : likeBeer.mutate(beerId); // 데이터 저장
+    if (!isLikedBeer) {
+      likeBeerMutation.mutate({ beerId: beerId, accessToken });
+    } else {
+      dislikeBeerMutation.mutate({ beerId: beerId, accessToken });
+    }
   };
 
   return (
@@ -97,9 +120,8 @@ export const DetailInfo: React.FC<DetailInfoProps> = ({
         <HStack w="full" justifyContent="space-between">
           <Text textStyle="h1">{beerName}</Text>
           <HStack gap="20px">
-            {/* like button */}
             <LikeButton
-              isLiked={isLiked}
+              isLiked={isLikedBeer}
               onClick={handleClickLike}
               w="40px"
               h="40px"
