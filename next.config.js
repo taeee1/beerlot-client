39d407e52acf840a3650157b77824dcc2440110d
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-01-21 22:28:00 +0900

    Feat: add library

--- next.config.js
+++ next.config.js
@@ -8,11 +8,18 @@ module.exports = {
       },
     ];
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
+
     return config;
   },
 };
