d844985f37d90a633eaa6094c6c9b015a8698385
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-03-03 18:21:54 +0900

    feat: remove window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
-import {ChakraProvider} from "@chakra-ui/react";
-import type {AppProps} from "next/app";
-import {ErrorBoundary} from "react-error-boundary";
-import {Hydrate, QueryClient, QueryClientProvider} from "react-query";
-import {RecoilRoot} from "recoil";
-import {BottomNav} from "../src/components/shared/BottomNav";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { ErrorBoundary } from "react-error-boundary";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { BottomNav } from "../src/components/shared/BottomNav";
 import "../styles/globals.css";
-import {theme} from "../styles/theme";
+import { theme } from "../styles/theme";
+import ReactGA from "react-ga";
 
-function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient();
+  // TODO: move to env
+  const REACT_APP_GA_TRACKING_ID = "G-TTNFK5BWQG";
+
+  const gaTrackingId = REACT_APP_GA_TRACKING_ID;
+  ReactGA.initialize(gaTrackingId, { debug: true });
+  ReactGA.pageview("beerlot GA test");
 
   return (
     <QueryClientProvider client={queryClient}>
