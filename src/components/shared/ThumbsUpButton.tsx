6e0230e14f067394e38f3c0f12523f1db7b8fab2
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-05-26 16:24:48 +0900

    feat: optimistic UI added for liking feed

--- src/components/shared/ThumbsUpButton.tsx
+++ src/components/shared/ThumbsUpButton.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonProps, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { ThumbsUpIcon } from "./CustomIcons/customIcons";
 
 interface ThumbsUpButtonProps extends ButtonProps {
@@ -10,29 +10,46 @@ interface ThumbsUpButtonProps extends ButtonProps {
 export const ThumbsUpButton: React.FC<ThumbsUpButtonProps> = ({
   thumbsUpNumber,
   isLiked = false,
+  onClick,
   ...props
 }) => {
+  const [localLiked, setLocalLiked] = useState(isLiked);
+  const [localNumber, setLocalNumber] = useState(thumbsUpNumber);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setLocalLiked(!localLiked);
+    onClick?.(e);
+    if (localLiked) {
+      setLocalNumber(Math.max(localNumber - 1, 0));
+    } else {
+      setLocalNumber(localNumber + 1);
+    }
+  };
+
+  const number = localLiked ? thumbsUpNumber + 1 : thumbsUpNumber;
+
   return (
     <Button
       py={1}
       px={2}
       bg={"white"}
-      border={isLiked ? "1px solid orange" : "1px solid gray"}
+      border={localLiked ? "1px solid orange" : "1px solid gray"}
       borderRadius="30px"
       minW={"initial"}
       h={"fit-content"}
-      color={isLiked ? "orange.200" : "black.100"}
+      color={localLiked ? "orange.200" : "black.100"}
       gap={1}
       _hover={{}}
       _focus={{}}
+      onClick={handleClick}
       {...props}
     >
       <ThumbsUpIcon
         boxSize={"16px"}
-        color={isLiked ? "orange.200" : "black.100"}
+        color={localLiked ? "orange.200" : "black.100"}
         mr={0}
       />
-      <Text textStyle={"h3"}>{thumbsUpNumber}</Text>
+      <Text textStyle={"h3"}>{localNumber}</Text>
     </Button>
   );
 };
