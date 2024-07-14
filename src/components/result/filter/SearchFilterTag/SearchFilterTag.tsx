3174b45e09e656588c4c4e9cba82e64df9e30fde
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-07-14 15:38:04 +0900

    feat: stick tag on left

--- src/components/result/filter/SearchFilterTag/SearchFilterTag.tsx
+++ src/components/result/filter/SearchFilterTag/SearchFilterTag.tsx
@@ -3,7 +3,7 @@ import {
   RightChevron,
 } from "@/components/shared/CustomIcons/customIcons";
 import FilterTag from "@/components/shared/Filters/FilterTag";
-import { ButtonProps, Icon } from "@chakra-ui/react";
+import { Box, ButtonProps, Icon } from "@chakra-ui/react";
 import React from "react";
 import { CategoryFilterListType } from "../../../../../interface/types";
 import { checkIsSelectedCategoryTitle } from "../../../../../utils/array";
@@ -30,27 +30,29 @@ const SearchFilterTag: React.FC<SearchFilterTagProps> = ({
       : isSelected;
 
   return (
-    <FilterTag
-      tagText={title}
-      borderRadius="15px"
-      pl={2}
-      pr={0}
-      py="1.5px"
-      h="full"
-      alignItems={"center"}
-      justifyContent={"center"}
-      bg={selected ? "yellow.300" : "yellow.200"}
-      onClick={onClick}
-      flexShrink={0}
-      {...props}
-    >
-      <Icon
-        as={isFilterListOpen ? RightChevron : DownChevron}
-        w="19px"
-        h="19px"
-        color="black.100"
-      />
-    </FilterTag>
+    <Box position="sticky" left={0} bg="white" zIndex={1}>
+      <FilterTag
+        tagText={title}
+        borderRadius="15px"
+        pl={2}
+        pr={0}
+        py="1.5px"
+        h="full"
+        alignItems={"center"}
+        justifyContent={"center"}
+        bg={selected ? "yellow.300" : "yellow.200"}
+        onClick={onClick}
+        flexShrink={0}
+        {...props}
+      >
+        <Icon
+          as={isFilterListOpen ? RightChevron : DownChevron}
+          w="19px"
+          h="19px"
+          color="black.100"
+        />
+      </FilterTag>
+    </Box>
   );
 };
 
