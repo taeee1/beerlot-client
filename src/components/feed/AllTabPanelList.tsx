1d9d9de3b0ed7d22d3b094945df634a4bcaacdf2
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-09-29 18:33:17 +0900

    feat: infinite scrolling added

--- src/components/feed/AllTabPanelList.tsx
+++ src/components/feed/AllTabPanelList.tsx
@@ -8,6 +8,7 @@ import { FeedFilter } from './FeedFilter/FeedFilter'
 import { useAllReviewsInfiniteQuery } from '../../../hooks/reviews/useReview'
 import { FollowingTabPanelItem } from './TabPanelItem'
 import { ReviewTypeV2 } from '../../../typedef/review'
+import { InfiniteScrollWrapper } from '@components/shared/InfiniteScrollWrapper'
 
 export const AllTabPanelList = () => {
   const accessToken = Cookies.get('beerlot-oauth-auth-request') ?? ''
@@ -18,42 +19,43 @@ export const AllTabPanelList = () => {
   const allReviewsQuery = useAllReviewsInfiniteQuery({
     sort: selectedTag,
   })
-  console.log('allReviewsQuery', allReviewsQuery)
+
   const handleSelectTag = async (tag: ReviewSortEnum) => {
     setSelectedTag(tag)
   }
 
   const handleLoadMore = () => {
-    allReviewsQuery.fetchNextPage()
+    if (allReviewsQuery.hasNextPage && !allReviewsQuery.isFetchingNextPage) {
+      allReviewsQuery.fetchNextPage()
+    }
   }
 
   return (
     <Flex flexDirection='column' gap={'10px'} pb={'64px'}>
       <FeedFilter selectedTag={selectedTag} onClickTag={handleSelectTag} />
-      {/* Map through all pages of reviews */}
-      {allReviewsQuery.data?.pages.map((page) =>
-        page.contents.map((review: ReviewTypeV2) => (
-          <FollowingTabPanelItem
-            key={review.id}
-            isLiked={likedReviewsListQuery.data?.includes(review.id)}
-            reviewId={Number(review.id)}
-            content={review.content}
-            nickname={review.member.username}
-            reviewTime={review.updated_at}
-            beerName={review.beer.name}
-            rate={review.rate}
-            imageSrc={review.image_url}
-            likedCount={review.like_count}
-            isEditable={false}
-          />
-        ))
-      )}
-      {allReviewsQuery.isFetching && <div>Loading more...</div>}
-      {allReviewsQuery.hasNextPage && (
-        <button onClick={handleLoadMore} disabled={allReviewsQuery.isFetching}>
-          Load More
-        </button>
-      )}
+      <InfiniteScrollWrapper
+        handleLoadMore={handleLoadMore}
+        isFetching={allReviewsQuery.isFetchingNextPage}
+      >
+        {/* Map through all pages of reviews */}
+        {allReviewsQuery.data?.pages.map((page) =>
+          page.contents.map((review: ReviewTypeV2) => (
+            <FollowingTabPanelItem
+              key={review.id}
+              isLiked={likedReviewsListQuery.data?.includes(review.id)}
+              reviewId={Number(review.id)}
+              content={review.content}
+              nickname={review.member.username}
+              reviewTime={review.updated_at}
+              beerName={review.beer.name}
+              rate={review.rate}
+              imageSrc={review.image_url}
+              likedCount={review.like_count}
+              isEditable={false}
+            />
+          ))
+        )}
+      </InfiniteScrollWrapper>
     </Flex>
   )
 }
